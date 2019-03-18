c8d3fa1834b6831081f71677d22fad7f2d5a51fc
Hovig <hovigghazeljian@gmail.com>
2019-03-18 00:38:37 -0400

    employee add/edit/delete works with the db

--- EmpSettingsForm.Designer.cs
+++ EmpSettingsForm.Designer.cs
@@ -32,16 +32,20 @@
             this.txtEmpName = new System.Windows.Forms.TextBox();
             this.txtEmpPass = new System.Windows.Forms.TextBox();
             this.txtEmpPosition = new System.Windows.Forms.TextBox();
-            this.lblEmpName = new System.Windows.Forms.Label();
             this.lblEmpPass = new System.Windows.Forms.Label();
             this.lblEmpPosition = new System.Windows.Forms.Label();
             this.lblEmpAccessLevel = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdbtnAdmin = new System.Windows.Forms.RadioButton();
+            this.rdbtnRegEmp = new System.Windows.Forms.RadioButton();
             this.lblEmpAdd = new System.Windows.Forms.Button();
             this.lblEmpEdit = new System.Windows.Forms.Button();
             this.btnEmpDelete = new System.Windows.Forms.Button();
-            this.rdbtnRegEmp = new System.Windows.Forms.RadioButton();
-            this.rdbtnAdmin = new System.Windows.Forms.RadioButton();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.lblEmpName = new System.Windows.Forms.Label();
+            this.lblID = new System.Windows.Forms.Label();
+            this.btnClearBoxs = new System.Windows.Forms.Button();
+            this.lblIdValue = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvEmployees)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -53,41 +57,33 @@
             this.dgvEmployees.Name = "dgvEmployees";
             this.dgvEmployees.Size = new System.Drawing.Size(666, 441);
             this.dgvEmployees.TabIndex = 0;
+            this.dgvEmployees.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvEmployees_CellClick);
             // 
             // txtEmpName
             // 
-            this.txtEmpName.Location = new System.Drawing.Point(6, 37);
+            this.txtEmpName.Location = new System.Drawing.Point(9, 72);
             this.txtEmpName.Name = "txtEmpName";
             this.txtEmpName.Size = new System.Drawing.Size(236, 20);
             this.txtEmpName.TabIndex = 1;
             // 
             // txtEmpPass
             // 
-            this.txtEmpPass.Location = new System.Drawing.Point(6, 87);
+            this.txtEmpPass.Location = new System.Drawing.Point(9, 122);
             this.txtEmpPass.Name = "txtEmpPass";
             this.txtEmpPass.Size = new System.Drawing.Size(236, 20);
             this.txtEmpPass.TabIndex = 2;
             // 
             // txtEmpPosition
             // 
-            this.txtEmpPosition.Location = new System.Drawing.Point(6, 126);
+            this.txtEmpPosition.Location = new System.Drawing.Point(9, 161);
             this.txtEmpPosition.Name = "txtEmpPosition";
             this.txtEmpPosition.Size = new System.Drawing.Size(236, 20);
             this.txtEmpPosition.TabIndex = 3;
             // 
-            // lblEmpName
-            // 
-            this.lblEmpName.AutoSize = true;
-            this.lblEmpName.Location = new System.Drawing.Point(6, 16);
-            this.lblEmpName.Name = "lblEmpName";
-            this.lblEmpName.Size = new System.Drawing.Size(52, 13);
-            this.lblEmpName.TabIndex = 5;
-            this.lblEmpName.Text = "Full name";
-            // 
             // lblEmpPass
             // 
             this.lblEmpPass.AutoSize = true;
-            this.lblEmpPass.Location = new System.Drawing.Point(3, 67);
+            this.lblEmpPass.Location = new System.Drawing.Point(6, 102);
             this.lblEmpPass.Name = "lblEmpPass";
             this.lblEmpPass.Size = new System.Drawing.Size(53, 13);
             this.lblEmpPass.TabIndex = 6;
@@ -96,7 +92,7 @@
             // lblEmpPosition
             // 
             this.lblEmpPosition.AutoSize = true;
-            this.lblEmpPosition.Location = new System.Drawing.Point(4, 110);
+            this.lblEmpPosition.Location = new System.Drawing.Point(7, 145);
             this.lblEmpPosition.Name = "lblEmpPosition";
             this.lblEmpPosition.Size = new System.Drawing.Size(44, 13);
             this.lblEmpPosition.TabIndex = 7;
@@ -105,7 +101,7 @@
             // lblEmpAccessLevel
             // 
             this.lblEmpAccessLevel.AutoSize = true;
-            this.lblEmpAccessLevel.Location = new System.Drawing.Point(3, 152);
+            this.lblEmpAccessLevel.Location = new System.Drawing.Point(6, 187);
             this.lblEmpAccessLevel.Name = "lblEmpAccessLevel";
             this.lblEmpAccessLevel.Size = new System.Drawing.Size(71, 13);
             this.lblEmpAccessLevel.TabIndex = 8;
@@ -113,6 +109,8 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.lblIdValue);
+            this.groupBox1.Controls.Add(this.lblID);
             this.groupBox1.Controls.Add(this.rdbtnAdmin);
             this.groupBox1.Controls.Add(this.rdbtnRegEmp);
             this.groupBox1.Controls.Add(this.lblEmpAccessLevel);
@@ -124,14 +122,38 @@
             this.groupBox1.Controls.Add(this.txtEmpName);
             this.groupBox1.Location = new System.Drawing.Point(676, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(258, 214);
+            this.groupBox1.Size = new System.Drawing.Size(258, 241);
             this.groupBox1.TabIndex = 9;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Employee info Add/Edit";
             // 
+            // rdbtnAdmin
+            // 
+            this.rdbtnAdmin.AutoSize = true;
+            this.rdbtnAdmin.Location = new System.Drawing.Point(139, 213);
+            this.rdbtnAdmin.Name = "rdbtnAdmin";
+            this.rdbtnAdmin.Size = new System.Drawing.Size(54, 17);
+            this.rdbtnAdmin.TabIndex = 10;
+            this.rdbtnAdmin.TabStop = true;
+            this.rdbtnAdmin.Text = "Admin";
+            this.rdbtnAdmin.UseVisualStyleBackColor = true;
+            this.rdbtnAdmin.CheckedChanged += new System.EventHandler(this.rdbtnRegEmp_CheckedChanged);
+            // 
+            // rdbtnRegEmp
+            // 
+            this.rdbtnRegEmp.AutoSize = true;
+            this.rdbtnRegEmp.Location = new System.Drawing.Point(12, 213);
+            this.rdbtnRegEmp.Name = "rdbtnRegEmp";
+            this.rdbtnRegEmp.Size = new System.Drawing.Size(62, 17);
+            this.rdbtnRegEmp.TabIndex = 9;
+            this.rdbtnRegEmp.TabStop = true;
+            this.rdbtnRegEmp.Text = "Regular";
+            this.rdbtnRegEmp.UseVisualStyleBackColor = true;
+            this.rdbtnRegEmp.CheckedChanged += new System.EventHandler(this.rdbtnRegEmp_CheckedChanged);
+            // 
             // lblEmpAdd
             // 
-            this.lblEmpAdd.Location = new System.Drawing.Point(682, 242);
+            this.lblEmpAdd.Location = new System.Drawing.Point(682, 259);
             this.lblEmpAdd.Name = "lblEmpAdd";
             this.lblEmpAdd.Size = new System.Drawing.Size(75, 37);
             this.lblEmpAdd.TabIndex = 10;
@@ -141,51 +163,77 @@
             // 
             // lblEmpEdit
             // 
-            this.lblEmpEdit.Location = new System.Drawing.Point(763, 242);
+            this.lblEmpEdit.Location = new System.Drawing.Point(762, 259);
             this.lblEmpEdit.Name = "lblEmpEdit";
             this.lblEmpEdit.Size = new System.Drawing.Size(75, 37);
             this.lblEmpEdit.TabIndex = 11;
             this.lblEmpEdit.Text = "Edit";
             this.lblEmpEdit.UseVisualStyleBackColor = true;
+            this.lblEmpEdit.Click += new System.EventHandler(this.lblEmpEdit_Click);
             // 
             // btnEmpDelete
             // 
-            this.btnEmpDelete.Location = new System.Drawing.Point(843, 242);
+            this.btnEmpDelete.Location = new System.Drawing.Point(843, 259);
             this.btnEmpDelete.Name = "btnEmpDelete";
             this.btnEmpDelete.Size = new System.Drawing.Size(75, 37);
             this.btnEmpDelete.TabIndex = 12;
             this.btnEmpDelete.Text = "Delete";
             this.btnEmpDelete.UseVisualStyleBackColor = true;
+            this.btnEmpDelete.Click += new System.EventHandler(this.btnEmpDelete_Click);
             // 
-            // rdbtnRegEmp
+            // btnBack
             // 
-            this.rdbtnRegEmp.AutoSize = true;
-            this.rdbtnRegEmp.Location = new System.Drawing.Point(9, 178);
-            this.rdbtnRegEmp.Name = "rdbtnRegEmp";
-            this.rdbtnRegEmp.Size = new System.Drawing.Size(62, 17);
-            this.rdbtnRegEmp.TabIndex = 9;
-            this.rdbtnRegEmp.TabStop = true;
-            this.rdbtnRegEmp.Text = "Regular";
-            this.rdbtnRegEmp.UseVisualStyleBackColor = true;
-            this.rdbtnRegEmp.CheckedChanged += new System.EventHandler(this.rdbtnRegEmp_CheckedChanged);
+            this.btnBack.Location = new System.Drawing.Point(685, 405);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(115, 40);
+            this.btnBack.TabIndex = 13;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
-            // rdbtnAdmin
+            // lblEmpName
             // 
-            this.rdbtnAdmin.AutoSize = true;
-            this.rdbtnAdmin.Location = new System.Drawing.Point(136, 178);
-            this.rdbtnAdmin.Name = "rdbtnAdmin";
-            this.rdbtnAdmin.Size = new System.Drawing.Size(54, 17);
-            this.rdbtnAdmin.TabIndex = 10;
-            this.rdbtnAdmin.TabStop = true;
-            this.rdbtnAdmin.Text = "Admin";
-            this.rdbtnAdmin.UseVisualStyleBackColor = true;
-            this.rdbtnAdmin.CheckedChanged += new System.EventHandler(this.rdbtnRegEmp_CheckedChanged);
+            this.lblEmpName.AutoSize = true;
+            this.lblEmpName.Location = new System.Drawing.Point(9, 51);
+            this.lblEmpName.Name = "lblEmpName";
+            this.lblEmpName.Size = new System.Drawing.Size(52, 13);
+            this.lblEmpName.TabIndex = 5;
+            this.lblEmpName.Text = "Full name";
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(9, 27);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(18, 13);
+            this.lblID.TabIndex = 12;
+            this.lblID.Text = "ID";
+            // 
+            // btnClearBoxs
+            // 
+            this.btnClearBoxs.Location = new System.Drawing.Point(682, 302);
+            this.btnClearBoxs.Name = "btnClearBoxs";
+            this.btnClearBoxs.Size = new System.Drawing.Size(82, 43);
+            this.btnClearBoxs.TabIndex = 14;
+            this.btnClearBoxs.Text = "Clear info of textboxs";
+            this.btnClearBoxs.UseVisualStyleBackColor = true;
+            this.btnClearBoxs.Click += new System.EventHandler(this.btnClearBoxs_Click);
+            // 
+            // lblIdValue
+            // 
+            this.lblIdValue.AutoSize = true;
+            this.lblIdValue.Location = new System.Drawing.Point(33, 27);
+            this.lblIdValue.Name = "lblIdValue";
+            this.lblIdValue.Size = new System.Drawing.Size(0, 13);
+            this.lblIdValue.TabIndex = 13;
             // 
             // employee_settings_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(943, 450);
+            this.Controls.Add(this.btnClearBoxs);
+            this.Controls.Add(this.btnBack);
             this.Controls.Add(this.btnEmpDelete);
             this.Controls.Add(this.lblEmpEdit);
             this.Controls.Add(this.lblEmpAdd);
@@ -206,7 +254,6 @@
         private System.Windows.Forms.TextBox txtEmpName;
         private System.Windows.Forms.TextBox txtEmpPass;
         private System.Windows.Forms.TextBox txtEmpPosition;
-        private System.Windows.Forms.Label lblEmpName;
         private System.Windows.Forms.Label lblEmpPass;
         private System.Windows.Forms.Label lblEmpPosition;
         private System.Windows.Forms.Label lblEmpAccessLevel;
@@ -216,5 +263,10 @@
         private System.Windows.Forms.Button btnEmpDelete;
         private System.Windows.Forms.RadioButton rdbtnAdmin;
         private System.Windows.Forms.RadioButton rdbtnRegEmp;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Label lblEmpName;
+        private System.Windows.Forms.Button btnClearBoxs;
+        private System.Windows.Forms.Label lblIdValue;
     }
 }
--- EmpSettingsForm.cs
+++ EmpSettingsForm.cs
@@ -27,6 +27,7 @@ namespace MultiFaceRec
             string sqlstr = "SELECT * FROM employee";
             try
             {
+                vt.Clear();
                 OleDbDataAdapter dad = new OleDbDataAdapter(sqlstr, constr);
                 dad.Fill(vt);
                 dad.Dispose();
@@ -38,48 +39,136 @@ namespace MultiFaceRec
                 MessageBox.Show("Error " + ex);
             }
         }
-
+        
+        //Add new eployee
         private void lblEmpAdd_Click(object sender, EventArgs e)
         {
-            if(checkIfFilled())
-            {/*
-                OleDbConnection connect = new OleDbConnection();
-                connect.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=eyePOS_DB_.accdb;";   //relational path to sourse
-                connect.Open();
-                OleDbCommand command = new OleDbCommand();
-                command.Connection = connect;
-                command.CommandText = "INSERT INTO employee (Full_Name,position,password,access_level) VALUES (' " + txtEmpName.Text + " ',' " + txtEmpPosition.Text + " ',' " + txtEmpPass.Text + " ' , ' " + accessLevelSelected + " ')";
-                command.ExecuteNonQuery();*/
+            if (dialogBox("Are you sure that you want to add the entered employee " + lblIdValue.Text, "Add Employee"))
+            {
+                if (checkIfFilled())
+                {
+                    try
+                    {
+                        using (var con = new OleDbConnection())
+                        {
+                            con.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=eyePOS_DB_.accdb;";
+                            con.Open();
 
-                /*OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=eyePOS_DB_.accdb;");
-                con.Open();
-                OleDbCommand com = new OleDbCommand();
-                com.Connection = con;
-                com.CommandType = CommandType.Text;
-                com.CommandText = "INSERT INTO employee (Full_Name,position,password,access_level) VALUES (' " + txtEmpName.Text + " ',' " + txtEmpPosition.Text + " ',' " + txtEmpPass.Text + " ' , ' " + accessLevelSelected + " ')";
-
-                com.ExecuteNonQuery();
-                MessageBox.Show("Saved");
-                con.Close();
-                */
-
-                OleDbConnection connect = new OleDbConnection();
-                connect.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=eyePOS_DB_.accdb;";   //relational path to sourse
-                connect.Open();
-                OleDbCommand command = new OleDbCommand();
-                command.Connection = connect;
-                command.CommandText = "INSERT INTO employee (Full_Name,position) VALUES ('cc','caa' )";
-                command.ExecuteNonQuery();
+                            using (var com = new OleDbCommand())
+                            {
+                                com.Connection = con;
+                                com.CommandText = "INSERT INTO employee ([Full_Name],[position],[password],[access_level]) VALUES (@Full_Name,@position,@password,@access_level)";
+                                com.Parameters.AddWithValue("@Full_Name", txtEmpName.Text);
+                                com.Parameters.AddWithValue("@position", txtEmpPosition.Text);
+                                com.Parameters.AddWithValue("@password", txtEmpPass.Text);
+                                com.Parameters.AddWithValue("@access_level", whichRBTIsChecked());
+                                com.ExecuteNonQuery();
+                            }
+                        }
+                        MessageBox.Show("Added, New employee");
+                        displayDataGridView();
+                        clearSelections();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Not saved: " + ex.Message);
+                    }
+                }
+                else
+                    MessageBox.Show("Fill all fields");
             }
-            else
-                MessageBox.Show("Fill all fields");
-
         }
+
+        //Edite existing Employee
+        private void lblEmpEdit_Click(object sender, EventArgs e)
+        {
+            if (dialogBox("Are you sure that you want to edit sellected employee " + lblIdValue.Text, "Edit Employee"))
+            {
+                try
+                {
+                    using (var con = new OleDbConnection())
+                    {
+                        con.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=eyePOS_DB_.accdb;";
+                        con.Open();
+
+                        using (var com = new OleDbCommand())
+                        {
+                            com.Connection = con;
+                            com.CommandText = "UPDATE employee SET [Full_Name] = @Full_Name, [position] = @position , [password] = @password, [access_level] = @access_level WHERE [ID] = @ID;";
+                            com.Parameters.AddWithValue("@Full_Name", txtEmpName.Text);
+                            com.Parameters.AddWithValue("@position", txtEmpPosition.Text);
+                            com.Parameters.AddWithValue("@password", txtEmpPass.Text);
+                            com.Parameters.AddWithValue("@access_level", whichRBTIsChecked());
+                            com.Parameters.AddWithValue("@ID", lblIdValue.Text);
+                            com.ExecuteNonQuery();
+                        }
+                    }
+                    MessageBox.Show("Edited, employee " + lblIdValue.Text);
+                    displayDataGridView();
+                    clearSelections();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Not saved: " + ex.Message);
+                }
+            }
+        }
+
+        //Delete exiting employee
+        private void btnEmpDelete_Click(object sender, EventArgs e)
+        {
+            if (dialogBox("Are you sure that you want to delete employee " + lblIdValue.Text, "Delete Employee"))
+            {
+                try
+                {
+                    using (var con = new OleDbConnection())
+                    {
+                        con.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=eyePOS_DB_.accdb;";
+                        con.Open();
+
+                        using (var com = new OleDbCommand())
+                        {
+                            com.Connection = con;
+                            com.CommandText = "DELETE FROM employee WHERE [ID] = @ID;";
+                            com.Parameters.AddWithValue("@ID", lblIdValue.Text);
+                            com.ExecuteNonQuery();
+                        }
+                    }
+
+                    MessageBox.Show("Deleted successfully, employee " + lblIdValue.Text);
+                    displayDataGridView();
+                    clearSelections();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Not saved: " + ex.Message);
+                }
+            }
+        }
+
+        public void clearSelections()
+        {
+            txtEmpName.Text = "";
+            txtEmpPass.Text = "";
+            txtEmpPosition.Text = "";
+            rdbtnAdmin.Checked = false;
+            rdbtnRegEmp.Checked = true;
+        }
+
         public bool checkIfFilled()
         {
             if (txtEmpName.Text == "" || txtEmpPass.Text == "" || txtEmpPosition.Text == "" || accessLevelSelected == "")
                 return false;
             return true;
+        }
+
+        public string whichRBTIsChecked()
+        {
+            if (rdbtnAdmin.Checked)
+                return "admin";
+            else if (rdbtnRegEmp.Checked)
+                return "regular";
+            return "not selected";
         }
 
         private void rdbtnRegEmp_CheckedChanged(object sender, EventArgs e)
@@ -89,7 +178,91 @@ namespace MultiFaceRec
             else if (rdbtnAdmin.Checked == true && rdbtnRegEmp.Checked == false)
                 accessLevelSelected = "admin";
         }
+
+
+        
+
+        private void dgvEmployees_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int index = e.RowIndex;// get the Row Index
+            DataGridViewRow selectedRow = dgvEmployees.Rows[index];
+            lblIdValue.Text = selectedRow.Cells[0].Value.ToString();
+            txtEmpName.Text = selectedRow.Cells[1].Value.ToString();
+            txtEmpPosition.Text = selectedRow.Cells[2].Value.ToString();
+            txtEmpPass.Text = selectedRow.Cells[3].Value.ToString();
+            if (selectedRow.Cells[4].Value.ToString() == "admin")
+            {
+                rdbtnAdmin.Checked = true;
+                rdbtnRegEmp.Checked = false;
+            }
+            else if (selectedRow.Cells[4].Value.ToString() == "regular")
+            {
+                rdbtnAdmin.Checked = false;
+                rdbtnRegEmp.Checked = true;
+            }
+            else 
+            {
+                rdbtnAdmin.Checked = false;
+                rdbtnRegEmp.Checked = false;
+            }
+
+
+
+        }
+
+        private void btnClearBoxs_Click(object sender, EventArgs e)
+        {
+            clearSelections();
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        public bool dialogBox(string message, string title)
+        {
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(message, title, buttons);
+            if (result == DialogResult.Yes)
+            {
+                return true;
+            }
+            else
+                MessageBox.Show("No Change", title);
+
+            return false;
+        }
+
     }
 
 
     }
+/*
+                OleDbConnection connect = new OleDbConnection();
+                connect.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=eyePOS_DB_.accdb;";   //relational path to sourse
+                connect.Open();
+                OleDbCommand command = new OleDbCommand();
+                command.Connection = connect;
+                command.CommandText = "INSERT INTO employee (Full_Name,position,password,access_level) VALUES (' " + txtEmpName.Text + " ',' " + txtEmpPosition.Text + " ',' " + txtEmpPass.Text + " ' , ' " + accessLevelSelected + " ')";
+                command.ExecuteNonQuery();*/
+
+/*OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=eyePOS_DB_.accdb;");
+con.Open();
+OleDbCommand com = new OleDbCommand();
+com.Connection = con;
+com.CommandType = CommandType.Text;
+com.CommandText = "INSERT INTO employee (Full_Name,position,password,access_level) VALUES (' " + txtEmpName.Text + " ',' " + txtEmpPosition.Text + " ',' " + txtEmpPass.Text + " ' , ' " + accessLevelSelected + " ')";
+
+com.ExecuteNonQuery();
+MessageBox.Show("Saved");
+con.Close();
+*/
+/*
+                OleDbConnection connect = new OleDbConnection();
+                connect.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=eyePOS_DB_.accdb;";   //relational path to sourse
+                connect.Open();
+                OleDbCommand command = new OleDbCommand();
+                command.Connection = connect;
+                command.CommandText = "INSERT INTO employee (Full_Name) VALUES (' " + txtEmpName.Text + " ')";
+                //"INSERT INTO employee(Full_Name, position, password, access_level) VALUES (, ' " + txtEmpPosition.Text + " ', ' " + txtEmpPass.Text + " ',' " + txtEmpName.Text + " ')";
+                command.ExecuteNonQuery();*/
